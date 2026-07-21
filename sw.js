820663609f26ca53bcdd1c36a3bd7123fd701ac2
juanfemondragon-art <juanfe.mondragon@gmail.com>
2026-07-21 09:45:43 -0500

    Update service worker to remove caching logic
    Refactor service worker to remove caching and handle fetch requests directly.

--- sw.js
+++ sw.js
@@ -1,43 +1,31 @@
-const CACHE_NAME = "plaquetas-icc-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./plaqueta_base.svg",
-  "./manifest.json",
-  "./icons/favicon.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
 self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
+    caches.keys()
+      .then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
             return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+          })
+        );
+      })
+      .then(function() {
+        return self.registration.unregister();
+      })
+      .then(function() {
+        return self.clients.matchAll();
+      })
+      .then(function(clients) {
+        clients.forEach(function(client) {
+          client.navigate(client.url);
+        });
+      })
   );
 });
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
